7c6af8421a907e24fb2d80551152f00fa6fc93d6
Lassmara <lassmara1906@gmail.com>
2025-04-14 00:59:01 +0530

    necessary files

--- models/Employer.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const EmployerSchema = new mongoose.Schema({
-  name: { type: String, unique: true },
-  isVerified: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('Employer', EmployerSchema);
